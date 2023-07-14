de1fec6271bcdbcb257fe2d5780c78ed6c2bd9b1
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2023-07-14 08:03:20 +0530

    bump deps

--- deps/std/path.ts
+++ deps/std/path.ts
@@ -1,1 +1,1 @@
-export { extname, fromFileUrl, join, relative } from "https://deno.land/std@0.193.0/path/mod.ts";
+export { extname, fromFileUrl, join, relative } from "https://deno.land/std@0.194.0/path/mod.ts";
